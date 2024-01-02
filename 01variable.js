ad011ffe84455a37f021e41f27e8a6b8b3e4f371
Manik Dixit <104916959+manikD1@users.noreply.github.com>
2024-01-02 06:04:15 +0530

    Update 01variable.js
    sample commit to check commit...

--- 01variable.js
+++ 01variable.js
@@ -1,5 +1,5 @@
 // 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
-
+// Sample comment to check commit
 
 let myName = 'manik';
 let flag = true;
